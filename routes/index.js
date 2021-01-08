36d49cf80c1d6fbf673f69ba96768d1906090629
Kyle Pykonen <kylepyk@gmail.com>
2021-01-07 16:52:50 -0800

    connected index.js to api routes/api

--- routes/index.js
+++ routes/index.js
@@ -1,2 +1,10 @@
 const path = require("path");
 const router = require("express").Router();
+const apiRs = require("./api");
+
+router.use("/api", apiRs);
+router.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
+  
+  module.exports = router;
