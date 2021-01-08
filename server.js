883249ee9af6a425e7dc243fd7b879ac3f357ab2
Kyle Pykonen <kylepyk@gmail.com>
2021-01-07 16:23:49 -0800

    added mongoose and routes requirements to server.js

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const routes = require("./routes");
+const mongoose = require("mongoose");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -18,6 +20,8 @@ if (process.env.NODE_ENV === "production") {
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
